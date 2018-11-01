da9851bfb6a9c407e393e752596f5f0fa1b1827a
Olivier ROLAND <newca12@users.noreply.github.com>
2018-11-01 13:54:12 +0100

    ch1.11-nombres_naturels_et_recursion

---
+++ ch1.11-nombres_naturels_et_recursion/exampleProofs.v
@@ -0,0 +1,15 @@
+Print nat.
+
+Check S O.
+
+Compute match S (S O) with O => true | S p => false end.
+
+Compute match O with O => true | S p => false end.
+
+Compute match S (S O) with O => O | S p => p end.
+
+
+
+
+
+
