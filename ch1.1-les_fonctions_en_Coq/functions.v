e9416f9970aa9576734c94b9c46e7051d5c635bc
Olivier ROLAND <newca12@users.noreply.github.com>
2018-11-01 09:34:01 +0100

    ch1.1-les_fonctions_en_Coq

---
+++ ch1.1-les_fonctions_en_Coq/functions.v
@@ -0,0 +1,21 @@
+Check fun x => x + 2.
+
+Check (fun x => x + 2) 3.
+
+Check 3.
+
+Compute (fun x => x + 2) 3.
+
+Compute (fun x => x + 2) 6.
+
+Definition add2 := fun x => x + 2.
+
+Check add2.
+
+Compute add2 3.
+
+Definition add2' x := x + 2.
+
+Check add2'.
+
+Compute add2' 6.
